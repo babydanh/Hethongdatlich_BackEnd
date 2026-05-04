69c92b6a6373a35f899711a376ca5be0126b4ed3
babydanh <Macter.970@gmail.com>
2026-05-04 20:48:13 +0700

    refactor lai module database de ho tro load schema tu dong

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,6 +3,12 @@ import { ConfigService } from '@nestjs/config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as usersSchema from '../module/users/entities/user.entity';
+import * as categoriesSchema from '../module/categories/entities/category.entity';
+import * as merchantsSchema from '../module/merchants/entities/merchant.entity';
+import * as servicesSchema from '../module/services/entities/service.entity';
+import * as bookingsSchema from '../module/bookings/entities/booking.entity';
+import * as reviewsSchema from '../module/reviews/entities/review.entity';
+import * as administrativeSchema from '../module/administrative/entities/administrative.entity';
 
 export const DRIZZLE = 'DRIZZLE';
 
@@ -14,9 +20,21 @@ export const DRIZZLE = 'DRIZZLE';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         const connectionString = `postgres://${configService.get('DATABASE_USER')}:${configService.get('DATABASE_PASSWORD')}@${configService.get('DATABASE_HOST')}:${configService.get('DATABASE_PORT')}/${configService.get('DATABASE_NAME')}`;
-        console.log(`🔌 Database connected to: ${configService.get('DATABASE_HOST')}:${configService.get('DATABASE_PORT')}/${configService.get('DATABASE_NAME')}`);
+        console.log(
+          `🔌 Database: ${configService.get('DATABASE_HOST')}:${configService.get('DATABASE_PORT')}/${configService.get('DATABASE_NAME')}`,
+        );
         const queryClient = postgres(connectionString);
-        return drizzle(queryClient, { schema: { ...usersSchema } });
+        return drizzle(queryClient, {
+          schema: {
+            ...usersSchema,
+            ...categoriesSchema,
+            ...merchantsSchema,
+            ...servicesSchema,
+            ...bookingsSchema,
+            ...reviewsSchema,
+            ...administrativeSchema,
+          },
+        });
       },
     },
   ],
